59ade09d1946a3758d353d25c9a004e86afec07e
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-03-31 22:28:35 -0400

    monopolistic firms introduced...
    these are price, wage setters. will take time to add relative firm market power from competition. forgive this effort until I do so.

--- firm.py
+++ firm.py
@@ -16,8 +16,8 @@ class Firm:
         # if a firm is IN an economy
         if self.eco:
             prod = sum([worker.productivity for worker in self.workers])
-            #print('prod', prod)
 
+            # if firm produces a good that is demanded by the economy
             if self.good in self.eco.goods_demanded.keys():
                 self.eco.goods_supplied[self.good] += prod
 
@@ -25,6 +25,21 @@ class Firm:
         # Worker is a citizen object
         self.workers.append(worker)
         worker.job = self.good
+
+    def set_price(self):
+        # Adds market price (since these firms are currently monopolies
+        # How does this happen?
+        pass
+
+    def set_wage(self):
+        # Sets a wage for every worker at that factory (I will make it 1)
+        for worker in self.workers:
+            wage = 1
+            worker.wage = wage
+
+    def pay_wage(self):
+        # Should this even be a function?
+        pass
 
 # There needs to be a way to hire all the citizens in an economy
 # Should they be randomly assigned to firm ids? Or competitive labor market...?
