93d51fa723171f404b8b0703c6877d1b6029c30a
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-03-31 23:03:22 -0400

    First step...
    I should've done this a while ago, but I want to make a nested key value stores my class instance attribute. Don't want to overload my constructor...

--- economy.py
+++ economy.py
@@ -20,8 +20,6 @@ class Economy:
         # Adds a specific citizen to the economy, adjusting demand accordingly
         self.citizens.append(citizen)
         self.add_demand()
-        # Hmmmmmm, this might be problematic later...
-        # self.adjust_demand()
 
     def introduce_citizens(self, n=1000):
         # Batch introduces n citizens to the economy
@@ -31,23 +29,25 @@ class Economy:
     def introduce_goods(self, goods_list):
         # Introduces goods to the economy, initializing supply at 0
         for good in goods_list:
+
+            # If something is to be modified, let's start here
             self.demand_weights[good] = np.random.default_rng().normal(1, 0.10, 1)[0]
             self.goods_demanded[good] = 0
             self.goods_supplied[good] = 0
+
 
     def add_demand(self):
         # This adds a consumer to already existing demand structure
         pop = len(self.citizens)
         for good in self.goods_demanded.keys():
-            self.goods_demanded[good]*= ((pop+1)/pop)
+            # Increases demand based off the addition of one citizen
+            self.goods_demanded[good] *= ((pop+1)/pop)
 
     def adjust_demand(self):
         # Adjusts demand based on population from base values
         population = len(self.citizens)
         for good, quantity_demanded in self.goods_demanded.items():
-            # print(len(self.goods_demanded))
             self.goods_demanded[good] = self.demand_weights[good] * population
-            # self.goods_demanded[good] = quantity_demanded
             self.demand_weights[good] = np.random.default_rng().normal(1, 0.05, 1)[0]
 
     def get_workforce(self):
@@ -87,7 +87,7 @@ class Economy:
             self.goods_supplied[good] = 0
 
     def pass_year(self):
-
+        # Passes a year for every citizen
         for citizen in self.citizens:
             citizen.pass_year()
 
@@ -104,7 +104,7 @@ class Economy:
 
         # Produces goods and sends report
         self.production_cycle()
-        self.report(age=True)
+        self.report(age=True, workforce=True)
 
         # Population consumes goods, zeroing supply
         self.consume_goods()
@@ -117,12 +117,16 @@ class Economy:
         if age:
             ages = [citizen.age for citizen in self.citizens]
             avg = sum(ages)/len(ages)
-            print(f'Average Citizen age {avg:.2f}')
+            print(f'{len(self.citizens)} citizens w/Average Citizen age {avg:.2f}')
 
         if workforce:
             workforce_ages = [worker.age for worker in self.workforce]
             avg = sum(workforce_ages)/len(workforce_ages)
-            print(f'Average Worker age {avg:.2f}')
+            print(f'{len(self.workforce)} workers w/ Average Worker age {avg:.2f}')
+
+
+
+
 
 
 
