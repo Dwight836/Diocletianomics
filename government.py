3c56a1a8e079f5183649c3d6c5be38fd9bc46d84
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-04-13 20:15:26 -0400

    adding government structure
    This might get purged if I can't do it well enough

---
+++ government.py
@@ -0,0 +1,16 @@
+# The state apparatus
+
+class Government:
+    def __init__(self):
+        self.defense_budget = 1
+
+    def tax(self, eco):
+        # currently taxes citizens only
+        pass
+
+    def defend(self, eco):
+        # Applies defense from budget
+        # eco.defense = 1
+        pass
+
+
