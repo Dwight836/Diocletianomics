35c242e6cd61b6cc5a6e726457b87b3281eb07c6
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-05-01 18:22:35 -0400

    wage / wealth split viz

--- driver.py
+++ driver.py
@@ -5,6 +5,7 @@ from firm import Firm
 import matplotlib.pyplot as plt
 import pandas as pd
 import pprint
+import seaborn as sns
 
 
 def main():
@@ -16,7 +17,7 @@ def main():
     eco = Economy()
 
     eco.introduce_goods(goods)
-    eco.introduce_citizens()
+    eco.introduce_citizens(n=1000)
     eco.introduce_firms()
     eco.introduce_workers()
 
@@ -25,69 +26,18 @@ def main():
 
     year = 285
 
-    industry_ls = []
-    pop_count = []
-    worker_count = []
-    employee_count = []
-
-    pop_count2 = []
-    worker_count2 = []
-    child_count = []
-
-    years = []
-
     while year < 320:
         eco.pass_year()
-        industry = 'bricks'
-
-        industry_ls.append(eco.goods[industry]['quantity_supplied'])
-
-        pop_count.append(len(eco.citizens))
-        worker_count.append(len(eco.workforce))
-
-        child_count.append(len([citizen for citizen in eco.citizens if citizen.age>18]))
-        employee_count.append([str(firm) for firm in eco.firms if firm.good == industry][0])
-
-        years.append(year)
         year += 1
 
-    #pprint.pp(employee_count)
-
-    x = years
-    y = pop_count
-    # y = industry_ls
-    y2 = worker_count
-
-    y3 = child_count
-
-    #percentage = [y2/y for y2,y in zip(y, y2)]
-    #print(percentage)
-    percentage = [(worker_pop/total_pop) for (worker_pop, total_pop)
-                  in zip(worker_count, pop_count)]
-
-    cc_percentage = [(child_count/total_pop) for (child_count, total_pop)
-                  in zip(child_count, pop_count)]
-
-    print(pop_count)
-    print()
-    print(child_count)
-    print()
-    print(percentage)
-
-    #plt.plot(x, percentage, color='seagreen', label='labor force participation rate')
-    #plt.plot(x, cc_percentage, color='coral', label='percent under 18')
-
-    plt.plot(x, y, color='red', label='population')
-    plt.plot(x, y2, color='black', label='workforce')
-    plt.plot(x, y3, color='lightblue', label='children')
-    #plt.plot
-
-    plt.xlabel('Year (CE)')
-    plt.ylabel('Y')
-    plt.title(f'Growth of Imperial X over Y')
-    plt.grid(axis='y')
-    plt.legend()
+    sns.set()
+    y1 = [citizen.balance for citizen in eco.citizens if citizen.age > 18]
+    y2 = [citizen.age for citizen in eco.citizens if citizen.age > 18]
+    y3 = [citizen.wage for citizen in eco.citizens if citizen.age > 18]
+    plt.scatter(x=y2, y=y1, c=y3, marker='.')
+    plt.colorbar()
     plt.show()
 
 
 main()
+
