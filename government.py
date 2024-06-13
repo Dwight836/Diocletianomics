d4c460bfe71cdf84645f1194606d6df94c9f247c
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-06-13 15:41:34 -0400

    connectivity

--- government.py
+++ government.py
@@ -5,9 +5,8 @@ import random as rnd
 class Government:
     def __init__(self, eco=None):
         self.eco = eco
-
         self.tax_rate = 0.05
-        self.budget = 0
+        self.budget = {'military': 0, 'administration': 0, 'other': 0}
         self.balance = 0
 
     def tax(self):
@@ -18,10 +17,8 @@ class Government:
             self.balance += owed
 
     def defend(self):
-        # Applies defense from budget, replace 1 w/ log reg type formula?
-        formula = None
-        self.eco.connectivity = 1
-        pass
+        # Sets bounded connectivity variable for defense (pos acceptable)
+        self.eco.connectivity = self.budget['military']**0.15
 
     def debt_relief(self):
         # Applies random debt relief, check for performance issues
@@ -32,7 +29,3 @@ class Government:
             relieved = rnd.choices([True, False], [0.01, 0.99], k=1)[0]
             if relieved:
                 debtor.balance = 0
-
-
-
-
