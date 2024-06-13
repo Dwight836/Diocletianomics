83a81046b21c4f09093356a7b8385feaac4b88c3
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-06-13 15:28:14 -0400

    business aristocracy

--- aristocrat.py
+++ aristocrat.py
@@ -8,14 +8,24 @@ class Aristocrat(Citizen):
     def __init__(self):
         super().__init__()
 
-        # placeholder balance...
-        # self.balance = rnd.uniform(1_000, 10_000)
+        # self.balance = rnd.uniform(1_000, 10_000) // placeholder
         self.balance = 1000
+        self.job = 'nobility'
+
+    def demote(self):
+        if self.balance < -1000:
+            self.job = None
 
     def create_firm(self):
-        self.balance -= 1000
-        fr = Firm(good=self.job, owner=self)
-        return fr
+        # This is for performance. Adjusted as needed.
+        if self.job == 'nobility' and self.balance <= 1000:
+            pass
+
+
+
+
+
+
 
 
 
