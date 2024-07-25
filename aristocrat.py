e6eb3aeb4dab70163c9c52191a2eefb878b7e9f4
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-07-25 10:03:37 -0400

    aristocrat object interfaces directly with eco

--- aristocrat.py
+++ aristocrat.py
@@ -5,21 +5,26 @@ import random as rnd
 
 
 class Aristocrat(Citizen):
-    def __init__(self):
+    def __init__(self, eco=None):
         super().__init__()
 
-        # self.balance = rnd.uniform(1_000, 10_000) // placeholder
-        self.balance = 1000
+        self.eco = None
+        self.balance = rnd.uniform(1_000, 10_000)
         self.job = 'nobility'
+        self.property = []
 
     def demote(self):
+        # Sets
         if self.balance < -1000:
             self.job = None
+            self.property = list()
 
     def create_firm(self):
-        # This is for performance. Adjusted as needed.
-        if self.job == 'nobility' and self.balance <= 1000:
-            pass
+        # Aristocrats create firm into eco interface!
+        if self.job == 'nobility' and self.balance <= 1000 and self.eco:
+            good = [self.eco['goods'].keys()][0]
+            self.eco.introduce_firm(good=good, eco=self.eco)
+            self.balance -= 1000
 
 
 
