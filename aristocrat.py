be4b42c40ef15b9e3a9a31b3450334a952372d78
Dwight836 <117865245+Dwight836@users.noreply.github.com>
2024-07-25 10:35:24 -0400

    better firm creation / property

--- aristocrat.py
+++ aristocrat.py
@@ -8,23 +8,27 @@ class Aristocrat(Citizen):
     def __init__(self, eco=None):
         super().__init__()
 
+        if self.parent and not eco:
+            self.eco = self.parent.eco
+
         self.eco = eco
         self.balance = rnd.uniform(1_000, 10_000)
         self.job = 'nobility'
         self.property = []
 
     def demote(self):
-        # Sets
+        # Going bankrupt / losing all property
         if self.balance < -1000:
             self.job = None
             self.property = list()
 
     def create_firm(self):
         # Aristocrats create firm into eco interface!
-        if self.job == 'nobility' and self.balance <= 1000 and self.eco:
-            good = [self.eco['goods'].keys()][0]
+        #if self.job == 'nobility' and self.balance <= 1000 and self.eco:
+        if self.balance >= 1000 and self.eco:
+            investment_good = rnd.choices(list(self.eco.goods.keys()))[0]
             self.balance -= 1000
-            self.eco.introduce_firm(good=good, eco=self.eco)
+            self.eco.introduce_firm(good=investment_good, owner=self)
             self.property.append(self.eco.firms[-1])
 
 
